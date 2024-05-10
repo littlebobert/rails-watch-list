639db723364980dcda48d9604de8a351c9438e25
Justin Garcia <justin.garcia@gmail.com>
2024-05-10 12:37:12 +0900

    save

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,6 +25,6 @@ class ListsController < ApplicationController
   private
 
   def strong_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
